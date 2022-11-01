67566b048a0448af4fa6541b5d967683f4fe8530
Slobozhancky <46904434+Slobozhancky@users.noreply.github.com>
2022-11-01 22:56:42 +0200

    feact: add response and new component Pizzas

--- src/App.js
+++ src/App.js
@@ -4,11 +4,23 @@ import { Home, Cart } from "./pages";
 import { Route, Routes } from "react-router-dom";
 
 function App() {
+    let [pizzas, setPizzas] = React.useState([]);
+
+    React.useEffect(() => {
+        fetch("http://localhost:3000/react-pizza/bd.json")
+            .then((response) => {
+                return response.json();
+            })
+            .then((data) => {
+                setPizzas(data.pizzas);
+            });
+    }, []);
+
     return (
         <div className="wrapper">
             <Header />
             <Routes>
-                <Route path="/" element={<Home />} />
+                <Route path="/react-pizza" element={<Home pizzas={pizzas} />} />
                 <Route path="cart" element={<Cart />} />
             </Routes>
         </div>
